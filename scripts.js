29ec0d2fac6d041e52e5a0299327e2ea9fc2598f
mingma <e.mingma@gmail.com>
2024-07-13 15:02:10 +0545

    updated

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Example JavaScript for additional interactivity
-
+<script>
 document.addEventListener('DOMContentLoaded', () => {
     // Smooth scroll to sections
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -11,3 +11,19 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', () => { const textElement = document.querySelector('.scrolling-text');
+	const roles = ["Web developer","Software Developer", "Computer Trainer", "Rapper"];
+	let index = 0;
+	
+	function updateText(){
+		textElement.style.animation = 'none';
+		textElement.offsetHeight;
+		textElement.style.animation = '';
+		textElement.textContent = roles[index]
+		index = (index+1)% roles.length;
+	}
+	setInterval(updateText, 2000)
+});
+</script>

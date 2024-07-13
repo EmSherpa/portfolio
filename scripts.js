111b293b9f18352bc5d2c16a906ef707f5dc95d0
mingma <e.mingma@gmail.com>
2024-07-13 18:55:10 +0545

    updates

--- scripts.js
+++ scripts.js
@@ -13,17 +13,4 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-document.addEventListener('DOMContentLoaded', () => { const textElement = document.querySelector('.scrolling-text');
-	const roles = ["Web developer","Software Developer", "Computer Trainer", "Rapper"];
-	let index = 0;
-	
-	function updateText(){
-		textElement.style.animation = 'none';
-		textElement.offsetHeight;
-		textElement.style.animation = '';
-		textElement.textContent = roles[index]
-		index = (index+1)% roles.length;
-	}
-	setInterval(updateText, 2000)
-});
 </script>
